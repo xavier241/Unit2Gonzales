6a443cdcc2a0d03cca503603514b9ecfabd2ac0d
xavier241 <Xaviergonzales009@gmail.com>
2025-03-24 10:21:53 -0700

    SpaceBar press
    ALomst done

--- BasicGamePlayGonzalesP2/Assets/Course Library/Scripts/PlayerController.cs
+++ BasicGamePlayGonzalesP2/Assets/Course Library/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerContyroller : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
+    public float xRange = 10.0f;
+    public GameObject projectilePrefab;
     
     void Start()
     {
@@ -15,6 +17,23 @@ public class PlayerContyroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x < xRange) 
+        {
+            transform.position = new Vector3(-xRange, transform.position.y,transform.position.z);
+        }
+        if (transform.position.x > xRange) 
+        {
+            transform.position = new Vector3(xRange,transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            // Launch a projectile from the player
+        }
+        if (transform.position.x < -10)
+        {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
