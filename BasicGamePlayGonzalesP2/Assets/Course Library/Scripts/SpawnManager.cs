100593168785003c916c0830dc7118bdc1658358
xavier241 <Xaviergonzales009@gmail.com>
2025-04-02 09:57:35 -0700

    Added triggers
    On Desttoryon Colision

--- BasicGamePlayGonzalesP2/Assets/Course Library/Scripts/SpawnManager.cs
+++ BasicGamePlayGonzalesP2/Assets/Course Library/Scripts/SpawnManager.cs
@@ -8,21 +8,18 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float SpawnRangeX = 20;
     private float spawnposZ = 20;
+    private float startdelay = 2;
+    private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal",startdelay,spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S)) 
-        {
-            SpawnRandomAnimal();
-        
-        }
     }
     void SpawnRandomAnimal() 
     {
