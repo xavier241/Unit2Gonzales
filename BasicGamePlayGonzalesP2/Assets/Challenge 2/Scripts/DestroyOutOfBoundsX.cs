e92ddf6e42a859d339b5fac15970f18ac7bd2c46
xavier241 <Xaviergonzales009@gmail.com>
2025-04-10 09:57:14 -0700

    Done
    Done

--- BasicGamePlayGonzalesP2/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
+++ BasicGamePlayGonzalesP2/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class DestroyOutOfBoundsX : MonoBehaviour
 {
-    private float leftLimit = 30;
-    private float bottomLimit = -5;
+    private float leftLimit = -30;
+    private float bottomLimit = -25;
 
     // Update is called once per frame
     void Update()
     {
         // Destroy dogs if x position less than left limit
-        if (transform.position.x > leftLimit)
+        if (transform.position.x < leftLimit)
         {
             Destroy(gameObject);
         } 
